da5a993ae540a30f3f44ad86e15bc40dcbc2090b
Michał Oleniacz <michal@MBP.local>
2022-12-16 20:43:22 +0100

    feat: HTTP and Auth service update

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,29 +1,56 @@
 import http from './httpService'
 
 const AUTH_URL = ''
+export interface ILoginResponse {
+  code:         string;
+  message:      string;
+  token:        string;
+  refreshToken: string;
+  id:           string;
+  nickname:     string;
+  email:        string;
+}
 
-type LoginResponse = any
 
-export async function login(username: string, password: string) {
+export interface IRegisterResponse {
+  code:    string;
+  message: string;
+  Id:      string;
+}
+
+type TAccountType = "kid" | "adult"
+
+type TLoginInput = {
+  email: string;
+  password: string;
+}
+
+type TRegisterInput = {
+    username: string,
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string
+    passwordConfirm: string,
+    type: TAccountType
+}
+
+export async function login(data: TLoginInput) {
   try {
-    const res = await http.post<LoginResponse>(AUTH_URL + '/login', {
-      body: JSON.stringify({ username, password }),
+    const res = await http.post<ILoginResponse>(AUTH_URL + '/login', {
+      body: JSON.stringify(data),
     })
   } catch (e) {
     if (e instanceof Error) console.log(e.message)
   }
 }
 
-type RegisterResponse = any
-
 export async function register(
-  username: string,
-  email: string,
-  password: string
+data: TRegisterInput
 ) {
   try {
-    const res = await http.post<RegisterResponse>(AUTH_URL + '/register', {
-      body: JSON.stringify({ username, email, password }),
+    const res = await http.post<IRegisterResponse>(AUTH_URL + '/register', {
+      body: JSON.stringify(data),
     })
   } catch (e) {
     if (e instanceof Error) console.error(e.message)
